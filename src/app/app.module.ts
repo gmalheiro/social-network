6c9adbf476cbb51c75f72fe151d5e7011afb7ec7
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-01 01:04:27 -0300

    added hero banner to header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeroBanner } from './components/hero-banner/hero-banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeroBanner
   ],
   imports: [
     BrowserModule,
