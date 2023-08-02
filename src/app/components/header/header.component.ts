e7106728de9a0fb44d1eb070731cdc261983d662
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-01 21:02:00 -0300

    Added banner with image and text to the project

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css','../../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class HeaderComponent {
-
+  logInConsole(){
+    console.log('Hey');
+  }
 }
